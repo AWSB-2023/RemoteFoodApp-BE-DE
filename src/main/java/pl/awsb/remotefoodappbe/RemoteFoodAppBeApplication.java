f9e4b231df19938014999e3532316d3caaa5f5f2
milypol98 <milypol98@gmail.com>
2023-11-10 00:30:41 +0100

    Pierwszy commit

---
+++ src/main/java/pl/awsb/remotefoodappbe/RemoteFoodAppBeApplication.java
@@ -0,0 +1,13 @@
+package pl.awsb.remotefoodappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteFoodAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteFoodAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/awsb/remotefoodappbe/RemoteFoodAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.awsb.remotefoodappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteFoodAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
